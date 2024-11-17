b7a0c4fc716d4f7b39e0a996da731d17bbd07f01
Andrei Zarmin De Jesus <161214656+DreiZzzzz@users.noreply.github.com>
2024-11-17 16:50:20 +0800

    Add files via upload

---
+++ TrainManagementApp/src/java/TrainManagement/UpdateTrainLine.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TrainManagement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class UpdateTrainLine {
+    public int train_line_id;
+    public String is_operational; 
+    
+    // empty constructor
+    public UpdateTrainLine() {
+        
+    }
+    
+    public int register_updateTrainLine() {
+        int temp = -1; 
+        
+           return 0; 
+    }
+    
+    
+    
+    
+    
+}
